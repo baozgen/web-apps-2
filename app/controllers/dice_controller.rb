fda1cadbcac25780bcbee122f4d87ccfcbbd73ed
baozgen <baris.ozgen@kellogg.northwestern.edu>
2023-02-14 20:30:23 +0000

    adds username feature

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -4,6 +4,7 @@ class DiceController < ApplicationController
         @die1 = rand(1..6)
         @die2 = rand(1..6)
         @total = @die1 + @die2
+        @username = params["username"]
     # instance method: with @
     # render the file in view/dice/index.html.erb
     # it's in there but we don't see it
