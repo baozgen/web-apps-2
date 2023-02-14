f7ecae67c3c4c7be27a5bd61f1653623090365a8
baozgen <baris.ozgen@kellogg.northwestern.edu>
2023-02-14 22:16:38 +0000

    tacosgram launch

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,25 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+
+  def new
+    @post = Post.new
+  end
+
+
+  def create
+    @posts = Post.new
+    @posts["author"] = params["post"]["author"]
+    @posts["body"] = params["post"]["body"]
+    @posts["image"] = params["post"]["image"]
+    @posts.save
+    redirect_to "/posts"
+  end
+
+  def destroy
+    #find post find_by
+    #redirect
+
+  end 
+
 end
